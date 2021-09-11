5b5cc756df5f11d6f40c92022b8a026645cb927b
Rolf Britten <rolfbritten@gmail.com>
2021-09-11 16:51:19 +0100

    sorting out issues and extra boilerplate

--- src/versioning.py
+++ src/versioning.py
@@ -1,50 +1,64 @@
-class gitControl():
+import subprocess
+
+
+class GitControl:
     """
     https://stackoverflow.com/questions/14989858/get-the-current-git-hash-in-a-python-script
     """
 
-    def __init__(strict: bool=True, verbose: bool = False):
+    def __init__(self, strict: bool=True, verbose: bool = False):
+        self.verbose = verbose
         self.label = self.repo_current_label()
         self.full_hash = self.repo_current_hash()
-        self.branch = repo_current_branch()
+        self.branch = self.repo_current_branch()
         self.stamp = self.label + self.branch
+        self.print_description = self.description()
+
+        if self.verbose:
+            print(self)
+
+    def __repr__(self):
+        return "GitControl() " + self.stamp
+
+    def __str__(self):
+        return self.    description()
+
+    def description(self):
+        return """Current Repo State:\n > Branch:\t{b}\n > Label:\t{l}\n > Hash:\t{h}
+        """.format(b=self.branch, l=self.label, h=self.full_hash)
 
     @staticmethod
     def repo_current_label():
         try:
-            commit = subprocess.check_output(
+            label = subprocess.check_output(
                 ['git', 'describe', '--always']).decode().strip()
         # Not copying .git folder into docker container
         except subprocess.CalledProcessError:
-            commit = "0000000"
+            label = "0000000"
 
-        if verbose:
-            print(' > Git Hash: {}'.format(commit))
-
-        return commit
+        return label
 
     @staticmethod
     def repo_current_hash():
         try:
-            commit = subprocess.check_output(
+            hash = subprocess.check_output(
                 ['git', 'rev-parse', 'HEAD']).decode().strip()
         # Not copying .git folder into docker container
         except subprocess.CalledProcessError:
-            commit = "0000000"
+            hash = "0000000"
 
-        if verbose:
-            print(' > Git Hash: {}'.format(commit))
-
-        return commit
+        return hash
 
     @staticmethod
     def repo_current_branch():
         try:
-            commit = subprocess.check_output(
+            branch = subprocess.check_output(
                 ['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode().strip()
         # Not copying .git folder into docker container
         except subprocess.CalledProcessError:
-            commit = "0000000"
+            branch = "0000000"
+
+        return branch
 
     # TODO
     # def assert_no_uncommitted
